d821148b000beb68c14ec8b371b9e5902a295aa7
Diego Espinosa <diegoeespinosag18@gmail.com>
2024-10-23 12:41:34 -0500

    segundo commit

--- ejemplos-IDE/Ejemplo05/src/ejemplo05/Ejemplo05.java
+++ ejemplos-IDE/Ejemplo05/src/ejemplo05/Ejemplo05.java
@@ -17,6 +17,7 @@ public class Ejemplo05 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("----------------");
+          // Esta operacion es para realizar una division
         System.out.println(10 / 3);
         
         System.out.println("----------------");
@@ -27,15 +28,19 @@ public class Ejemplo05 {
         System.out.println(10 / (3.0 + 1));
         
         System.out.println("----------------");
+        // Esta operacion es para hacer el calculo del residuo
         System.out.println(10 % 3 );
         
         System.out.println("----------------");
+        // Esta operacion es para elevar un numero al exponenete ingresado
         System.out.println(Math.pow(10, 2));
         
         System.out.println("----------------");
+        // Este operacion es para poder sacar la raiz cuadrada
         System.out.println(Math.sqrt(16));
         
         double m = Math.sqrt(16);
+         // Este operacion es para poder sacar la raiz cuadradat
         System.out.println(m);
         
     }
